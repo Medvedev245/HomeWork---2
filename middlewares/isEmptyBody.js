3decb56405c1f29ae4212639863de89ebdc23453
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2023-12-18 21:15:38 +0100

    Initial commit

---
+++ middlewares/isEmptyBody.js
@@ -0,0 +1,17 @@
+import { HttpError } from "../Helpers/index.js";
+
+export const isEmptyBody = (req, res, next) => {
+  const { length } = Object.keys(req.body);
+  if (!length) {
+    return next(HttpError(400, "Body must have fields"));
+  }
+  next();
+};
+
+export const isEmptyBodyPut = (req, res, next) => {
+  const { length } = Object.keys(req.body);
+  if (!length) {
+    return next(HttpError(400, "missing fields"));
+  }
+  next();
+};
